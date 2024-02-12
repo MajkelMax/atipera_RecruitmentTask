1ab8b12929137c2858938cc3134f6012639253ed
Michał Grzelka <michal.grzelka0@gmail.com>
2024-02-12 17:29:09 +0100

    Start Project

---
+++ src/main/java/com/atipera/recruitmenttask/Application.java
@@ -0,0 +1,17 @@
+package com.atipera.recruitmenttask;
+
+import com.atipera.recruitmenttask.utils.GithubUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+
+}
